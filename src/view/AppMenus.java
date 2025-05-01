e6a747045863418adc2344924159bec1acee0934
Rodri <nunes90rodrigo@gmail.com>
2025-05-01 13:35:26 +0100

    Only socket v2

--- src/view/AppMenus.java
+++ src/view/AppMenus.java
@@ -16,75 +16,7 @@ public class AppMenus {
         System.out.print("=>");
     }
 
-    /**
-     * Função para mostrar o menu de utilizador
-     */
-    public void menuGestor(){
-        System.out.println("================= MENU GESTOR =================");
-        System.out.println("== 1- REGISTAR GESTOR                        ==");
-        System.out.println("==                                           ==");
-        System.out.println("== 2- LISTAR UTILIZADORES                    ==");
-        System.out.println("==                                           ==");
-        System.out.println("== 3- PESQUISAR UTILIZADORES                 ==");
-        System.out.println("==                                           ==");
-        System.out.println("== 4- VER NOTIFICAÇÕES                       ==");
-        System.out.println("==                                           ==");
-        System.out.println("== 5- VER CERTEFICAÇÕES                      ==");
-        System.out.println("==                                           ==");
-        System.out.println("== 6- PESQUISAR PEDIDOS                      ==");
-        System.out.println("==                                           ==");
-        System.out.println("== 7- VER ESTADO DE CERTIFICAÇÃO             ==");
-        System.out.println("==                                           ==");
-        System.out.println("== 8- PESQUISAR EQUIPAMENTOS                 ==");
-        System.out.println("==                                           ==");
-        System.out.println("== 9- ACEITAR/RECUSAR UTILIZADOR             ==");
-        System.out.println("==                                           ==");
-        System.out.println("== 10- REMOVER CONTA DE UTILIZADOR           ==");
-        System.out.println("==                                           ==");
-        System.out.println("== 11- ACEITAR PEDIDO CERTEFICAÇÃO           ==");
-        System.out.println("==                                           ==");
-        System.out.println("== 12- ADICIONAR LICENÇA EM APLICAÇÃO        ==");
-        System.out.println("==                                           ==");
-        System.out.println("== 13- ATRIBUIR LICENSA A CERTIFICAÇÃO       ==");
-        System.out.println("==                                           ==");
-        System.out.println("== 14- ALTERAR INFORMAÇÕES DE UTILIZADORES   ==");
-        System.out.println("==                                           ==");
-        System.out.println("== 15- VER LOGS DA APLICAÇÃO                 ==");
-        System.out.println("==                                           ==");
-        System.out.println("== 0- SAIR                                   ==");
-        System.out.println("===============================================");
-        System.out.print("=>");
-    }
 
-    /**
-     *
-     */
-    public void menuFabricante(){
-        System.out.println("============== MENU FABRICANTE ==============");
-        System.out.println("== 1- REGISTAR FABRICANTE                  ==");
-        System.out.println("==                                         ==");
-        System.out.println("== 2- ADICIONAR EQUIPAMENTO                ==");
-        System.out.println("==                                         ==");
-        System.out.println("== 3- PEDIR CERTEFICACAO PARA EQUIPAMENTO  ==");
-        System.out.println("==                                         ==");
-        System.out.println("== 4- LISTAR EQUIPAMENTOS                  ==");
-        System.out.println("==                                         ==");
-        System.out.println("== 5- LISTAR PEDIDOS FEITOS                ==");
-        System.out.println("==                                         ==");
-        System.out.println("== 6- PESQUISAR EQUIPAMENTOS               ==");
-        System.out.println("==                                         ==");
-        System.out.println("== 7- PESQUISAR PEDIDOS CERTIFICACAO       ==");
-        System.out.println("==                                         ==");
-        System.out.println("== 8- VER ESTADO DE UMA CERTIFICAÇÃO       ==");
-        System.out.println("==                                         ==");
-        System.out.println("== 9- REMOVER CONTA                        ==");
-        System.out.println("==                                         ==");
-        System.out.println("== 10- ALTERAR MINHAS INFOS                ==");
-        System.out.println("==                                         ==");
-        System.out.println("== 0- SAIR                                 ==");
-        System.out.println("=============================================");
-        System.out.print("=>");
-    }
 
     public void menuFabricanteCliente(){
         System.out.println("============== MENU FABRICANTE CLIENT ==============");
@@ -109,43 +41,6 @@ public class AppMenus {
         System.out.print("FACRICANTE (CLIENT)=>");
     }
 
-    /**
-     * Função para mostrar o menu de técnico
-     */
-    public void menuTecnico(){
-        System.out.println("====================== MENU TÉCNICO ======================");
-        System.out.println("== 1- REGISTAR TÉCNICO                                  ==");
-        System.out.println("==                                                      ==");
-        System.out.println("== 2- VER NOTIFICAÇÕES                                  ==");
-        System.out.println("==                                                      ==");
-        System.out.println("== 3- REMOVER CONTA                                     ==");
-        System.out.println("==                                                      ==");
-        System.out.println("== 4- VER CERTIFICAÇÕES                                 ==");
-        System.out.println("==                                                      ==");
-        System.out.println("== 5- INSPECIONAR EQUIPAMENTO                           ==");
-        System.out.println("==                                                      ==");
-        System.out.println("== 6- ACEITAR/NEGAR CERTIFICAÇÃO DE EQUIPAMENTO         ==");
-        System.out.println("==                                                      ==");
-        System.out.println("== 7- ALTERAR MINHAS INFOS                              ==");
-        System.out.println("==                                                      ==");
-        System.out.println("== 8- CANCELAR CERTIFICAÇÃO                             ==");
-        System.out.println("==                                                      ==");
-        System.out.println("== 0- SAIR                                              ==");
-        System.out.println("==========================================================");
-        System.out.print("=>");
-    }
-
-    /**
-     * Função para mostrar o menu de tipo
-     */
-    public void tipoMenu(){
-        System.out.println(" == SELECIONE O SEU TIPO: ==");
-        System.out.println(" == 1- Fabricante         ==");
-        System.out.println(" == 2- Técnico            ==");
-        System.out.println(" == 0- Sair               ==");
-        System.out.println(" ===========================");
-        System.out.print("=>");
-    }
 
     public void clientMenu(){
         System.out.println("============== CLIENT MENU ===============");
