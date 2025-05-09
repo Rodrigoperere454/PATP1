cb2f97d223a86587bfe67f1f8932d4657fd8d0ee
Rodri <nunes90rodrigo@gmail.com>
2025-05-09 17:24:40 +0100

    Refactor main method to establish database connection and initialize user menu

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,26 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import controller.DBconfig;
+import controller.UtilizadorController;
+import java.sql.Connection;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+public class Main {
+    /**
+     * Função principal, verifica a conexão com a base de dados e inicia o programa
+     * @param args
+     */
+    public static void main(String[] args) throws Exception {
+
+        Connection connection = null;
+
+        while (connection == null) {
+            connection = DBconfig.getConnection();
+            Thread.sleep(1000);
+            if (connection == null) {
+                System.out.println("Se é a sua primeira vez no programa este erro é normal, caso este erro seja persistente contacte o suporte(Professor Marco de PA).");
+                DBconfig.configurarBD();
+            }
         }
+
+        UtilizadorController runner = new UtilizadorController();
+        runner.menuInicial();
     }
 }
